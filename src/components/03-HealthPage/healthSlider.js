6d79aea11dcd7a6f04fb0065a8e1095bfb08eb1f
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2020-05-14 21:33:32 +0800

    Changes to HealthPage

--- src/components/03-HealthPage/healthSlider.js
+++ src/components/03-HealthPage/healthSlider.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react'
 class Slider extends Component {
 
 	state = {
-		sliderContents: [<Global />, <Philippines />, <Search />],
+		sliderContents: [Emergencies(), MentalHealth(), PhysicalHealth()],
 		x: 0
 	}
 
@@ -14,16 +14,48 @@ class Slider extends Component {
 
 	render() {
 		return (
-			<div class="container" id="statistics_slider">
+			<div class="container" id="health_slider">
 				{this.state.sliderContents.map((item, index) => {
 					return <div key={index} className="slide" style={{ transform: `translateX(${this.state.x}%)`}}> {item} </div>
 				})}
 				<button id="goLeft" onClick={this.goLeft}><i class="arrow left"></i></button>
 				<button id="goRight" onClick={this.goRight}><i class="arrow right"></i></button>
-				<a href="#newsIntro"><img class="downArrow" src="https://image.flaticon.com/icons/svg/2316/2316598.svg" /></a>
+				<a href="#healthResources"><img class="downArrow" src="https://image.flaticon.com/icons/svg/2316/2316598.svg" /></a>
 			</div>
 		)
 	}
 }
 
 export default Slider
+
+
+
+/*Emergencies*/
+function Emergencies() {
+	return (
+		<div id="caption">
+			<h1>Do not hesitate reaching out to hotlines whenever you are facing an emergency.</h1>
+			<button><a href="#emergencies">Resources</a></button>
+		</div>
+	)
+}
+
+/*Mental Health*/
+function MentalHealth() {
+	return (
+		<div id="caption">
+			<h1>Mental health is especially crucial in these times. Take care of it, and your future self will be thanking you.</h1>
+			<button><a href="#mentalHealth">Resources</a></button>
+		</div>
+	)
+}
+
+/*Physical Health*/
+function PhysicalHealth() {
+	return (
+		<div id="caption">
+			<h1>Staying physically healthy will help you feel good in the inside, and help relieve negativity from your system.</h1>
+			<button><a href="#physicalHealth">Resources</a></button>
+		</div>
+	)
+}
